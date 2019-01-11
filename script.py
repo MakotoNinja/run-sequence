01a31b341a2d93b0ed0970c21d428fcfb0554420
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-11 11:14:02 -1000

    fixed script.py

--- script.py
+++ script.py
@@ -5,11 +5,15 @@
 '''
 
 import os
+import requests
 from farmware_tools import device
 from farmware_tools import app
 from farmware_tools import get_config_value
 
-s_name = 'one'#get_config_value('Run Sequence', 'sequence_name')
+TOKEN =
+headers = {'Authorization': 'Bearer {}'.format(api_token), 'content-type': "application/json"}
+
+s_name = get_config_value('Run Sequence', 'sequence_name')
 f_name = 'two'#get_config_value('Run Sequence', 'farmware_name')
 
 log = "Sequence Name: {}, Farmware Name: {}".format(s_name, f_name)
