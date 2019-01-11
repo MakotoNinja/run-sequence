e3a16d18838184cdbabe717039cda457d62fcc26
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-11 10:50:26 -1000

    fixed script.py

--- script.py
+++ script.py
@@ -12,5 +12,5 @@ from farmware_tools import get_config_value
 s_name = get_config_value('Random Move Relative', 'sequence_name')
 f_name = get_config_value('Random Move Relative', 'farmware_name')
 
-log = Sequence Name: {}, Farmware NName: {}".format(s_name, f_name)
+log = "Sequence Name: {}, Farmware NName: {}".format(s_name, f_name)
 device.log(log, 'info', ['toast'])
