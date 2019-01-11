ba29fc0bd6c5d3301298f0117bbfdb231121642a
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-11 10:42:18 -1000

    Initial Commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-<h1>Random Move Relative</h1
-<p>Moves the UTM randomly within the provided constraints.</p>
-<p>Useful for making multiple stabs at a weed.</p>
+<h1>Run Sequence</h1
+<p>Testing how to run sequences and Farmwares</p>
--- script.py
+++ script.py
@@ -1,31 +1,16 @@
 #!/usr/bin/env python
 
 '''
- ' Single Axis
+ ' Testing running sequences and Farmwares
 '''
 
 import os
 from farmware_tools import device
 from farmware_tools import app
 from farmware_tools import get_config_value
-from random import randint
 
-# create Celery coordinate node
-coord = {
-	'x' : 0,
-	'y' : 0,
-	'z' : 0
-}
+s_name = get_config_value('Random Move Relative', (axis + 'sequence_name')
+f_name = get_config_value('Random Move Relative', (axis + 'farmware_name')
 
-# get input values
-for axis in coord:
-	lo = int(get_config_value('Random Move Relative', (axis + '_lo')))
- 	hi = int(get_config_value('Random Move Relative', (axis + '_hi')))
-	pos = randint(lo, hi)
-	if not randint(0, 1): pos *= -1
-	coord[axis] = pos
-
-log = "Moving relative: {}".format(coord)
+log = Sequence Name: {}, Farmware NName: {}".format(s_name, f_name)
 device.log(log, 'info', ['toast'])
-# perform the move
-device.move_relative(coord['x'], coord['y'], coord['z'], 100)
