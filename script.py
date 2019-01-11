731f87b6f94613da93a4c48b31a242bcb134386c
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-11 13:09:09 -1000

    try api token

--- script.py
+++ script.py
@@ -10,7 +10,12 @@ from farmware_tools import device
 from farmware_tools import app
 from farmware_tools import get_config_value
 
-TOKEN = os.environ['API_TOKEN']
+try:
+	api_token = os.environ['API_TOKEN']
+except KeyError:
+	api_token = 'Not Set'
+	device.log('API_TOKEN not set', message_type='error', title='Class API:api_setup')
+
 headers = {'Authorization': 'Bearer {}'.format(api_token), 'content-type': "application/json"}
 
 s_name = get_config_value('Run Sequence', 'sequence_name', str)
