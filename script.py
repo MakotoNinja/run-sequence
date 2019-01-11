3d92ef3e065bf0c0f08133cfc3fb8174424abaeb
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-11 10:56:38 -1000

    fixed script.py

--- script.py
+++ script.py
@@ -9,8 +9,8 @@ from farmware_tools import device
 from farmware_tools import app
 from farmware_tools import get_config_value
 
-s_name = get_config_value('Random Move Relative', 'sequence_name')
-f_name = get_config_value('Random Move Relative', 'farmware_name')
+s_name = get_config_value('Run Sequence', 'sequence_name')
+f_name = get_config_value('Run Sequence', 'farmware_name')
 
-log = "Sequence Name: {}, Farmware NName: {}".format(s_name, f_name)
+log = "Sequence Name: {}, Farmware Name: {}".format(s_name, f_name)
 device.log(log, 'info', ['toast'])
