d4a650f04299a74816434ec15aa094ae675c3b20
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-11 11:19:41 -1000

    add str type to get_config_value calls

--- script.py
+++ script.py
@@ -13,8 +13,8 @@ from farmware_tools import get_config_value
 #TOKEN = os.environ['API_TOKEN']
 #headers = {'Authorization': 'Bearer {}'.format(api_token), 'content-type': "application/json"}
 
-s_name = get_config_value('Run Sequence', 'sequence_name')
-f_name = 'two'#get_config_value('Run Sequence', 'farmware_name')
+s_name = get_config_value('Run Sequence', 'sequence_name', str)
+f_name = get_config_value('Run Sequence', 'farmware_name', str)
 
 log = "Sequence Name: {}, Farmware Name: {}".format(s_name, f_name)
 device.log(log, 'info', ['toast'])
