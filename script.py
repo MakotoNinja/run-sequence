0d18f326531792081f65aedaa05634a276f47e00
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-11 13:01:16 -1000

    checking API token

--- script.py
+++ script.py
@@ -10,11 +10,12 @@ from farmware_tools import device
 from farmware_tools import app
 from farmware_tools import get_config_value
 
-#TOKEN = os.environ['API_TOKEN']
-#headers = {'Authorization': 'Bearer {}'.format(api_token), 'content-type': "application/json"}
+TOKEN = os.environ['API_TOKEN']
+headers = {'Authorization': 'Bearer {}'.format(api_token), 'content-type': "application/json"}
 
 s_name = get_config_value('Run Sequence', 'sequence_name', str)
 f_name = get_config_value('Run Sequence', 'farmware_name', str)
 
 log = "Sequence Name: {}, Farmware Name: {}".format(s_name, f_name)
+log = 'API TOKEN: {}'.format(TOKEN)
 device.log(log, 'info', ['toast'])
