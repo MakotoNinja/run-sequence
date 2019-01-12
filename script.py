670275a253b52651c9bb8fca7a26c8e60220aaa1
MakotoNinja <wayne.makoto.sturdy@gmail.com>
2019-01-12 08:30:16 -1000

    loop farmware

--- script.py
+++ script.py
@@ -13,6 +13,7 @@ from farmware_tools import get_config_value
 
 s_name = get_config_value('Run Sequence', 'sequence_name', str)
 f_name = get_config_value('Run Sequence', 'farmware_name', str)
+loops = get_config_value('Run Sequence', 'loop_farmware', int)
 
 if len(''.join(s_name.split())) > 0:
 	try:
@@ -23,7 +24,8 @@ else :
 	device.log("No text was entered in sequence input.", 'error', ['toast'])
 
 if len(''.join(f_name.split())) > 0:
-	device.execute_script(label = f_name)
+	for i in range(loops):
+		device.execute_script(label = f_name)
 else :
 	device.log("No text was entered in farmware input.", 'error', ['toast'])
 
